97e9804a842a9583c36e8ea8e099be5784b57d1c
Sagar tayde <sagartayde201@gmail.com>
2025-07-08 21:39:56 +0530

    Add files via upload

---
+++ Area_Of_Circul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    float radius;
+
+    printf("Enter a number :");
+    scanf("%f",&radius);
+
+    float pi = 3.1415;
+    float area = pi* radius * radius;
+    
+    printf(" The area if circul is : %f",area);
+
+}
